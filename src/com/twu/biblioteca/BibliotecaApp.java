c6ba2dc5a2b7907f97835f2e2c33086fade492e9
Seb <sebastian.henn@gmail.com>
2015-07-16 16:38:17 +0800

    Print and test welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,13 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp testApp = new BibliotecaApp();
+        testApp.printWelcomeMessage();
+    }
+
+    public String printWelcomeMessage() {
+        String message = "Welcome to the Bangalore Public Library management system BIBLIOTECA!";
+        System.out.println(message);
+        return message;
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -4,10 +4,13 @@ package com.twu.biblioteca;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+
 public class ExampleTest {
 
+    BibliotecaApp testApp = new BibliotecaApp();
+
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testPrintWelcomeMessage() {
+        assertEquals("Welcome to the Bangalore Public Library management system BIBLIOTECA!", testApp.printWelcomeMessage());
     }
 }
